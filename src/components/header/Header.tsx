1b6caec67c8a59f4dc941bab342c973800c6b32f
mkoroleva5 <mkoroleva5@gmail.com>
2022-12-14 20:08:52 +0300

    feat: add header layout

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,7 +1,26 @@
-import style from "./Header.module.css"
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import style from './Header.module.css';
+import logoSource from '../../assets/images/logo.png';
+import cartImg from '../../assets/images/cart.png';
 
 export const Header = () => {
   return (
-    <div className={style.header}>Header</div>
-  )
-}
+    <div className={style.header_wrapper}>
+      <div className={style.header}>
+        <a href="#" className={style.logo_wrapper}>
+          <img className={style.logo} src={logoSource} alt="Logo" />
+          <div className={style.title}>healthy food</div>
+        </a>
+        <div className={style.cart_wrapper}>
+          <div className={style.total}>
+            <div>Итого:</div>
+            <div>0</div>
+          </div>
+          <div className={style.cart}>
+            <img className={style.cart_image} src={cartImg} alt="Cart" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client';
 import { App } from './App';
 import 'normalize.css';
 import './index.css';
+import { Header } from './components/header/Header';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />;
+    <Header />
+    <App />
   </React.StrictMode>,
 );
