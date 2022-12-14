bc8684889a108f3251c7c99e0fd117ec91a85df5
MOONcitizenX <mooncitizenx@gmail.com>
2022-12-14 20:31:12 +0300

    feat: add basic productCard layout and styles

--- src/components/productCard/ProductCard.tsx
+++ src/components/productCard/ProductCard.tsx
@@ -3,11 +3,20 @@ import style from './ProductCard.module.css';
 
 export const ProductCard = ({ title, stock, price, preview }: Partial<Product>) => {
   return (
-    <div className={style.card_wrapper}>
-      <img src={preview} alt={title} />
+    <div className={style.cardWrapper}>
+      <img src={preview} alt={title} className={style.img} />
       <h3 className={style.title}>{title}</h3>
-      <p className={style.stock}>{stock}</p>
-      <p className={style.price}>{price}</p>
+      <p className={style.stock}>В наличии: {stock}</p>
+      <p className={style.price}>
+        {`${price}`
+          .split('.')
+          .map((el, ind) => (ind === 1 ? el.padEnd(2, '0') : el))
+          .join(',')}{' '}
+        BYN
+      </p>
+      <button type="button" className={style.button}>
+        В корзину
+      </button>
     </div>
   );
 };
