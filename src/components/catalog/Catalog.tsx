de60ed73bcf1adf23336ca1e4721f17bdffb1495
mkoroleva5 <mkoroleva5@gmail.com>
2022-12-15 20:09:18 +0300

    feat: add switching layout for catalog wrapper

--- src/components/catalog/Catalog.tsx
+++ src/components/catalog/Catalog.tsx
@@ -1,11 +1,23 @@
+import { useState } from 'react';
 import style from './Catalog.module.css';
 import { ProductCard } from '../productCard/ProductCard';
 import products from '../../data/products.json';
 
+export type DisplayLayout = 'table' | 'list';
+
 export const Catalog = () => {
+  const [layout, setLayout] = useState('table');
   return (
     <main className={style.main}>
-      <div className={style.catalogWrapper}>
+      <div>
+        <button type="button" onClick={() => setLayout('table')}>
+          1
+        </button>
+        <button type="button" onClick={() => setLayout('list')}>
+          2
+        </button>
+      </div>
+      <div className={`${style.catalogWrapper} ${layout === 'table' ? '' : style.list}`}>
         {products.map((item) => {
           return (
             <ProductCard
@@ -15,6 +27,7 @@ export const Catalog = () => {
               preview={item.preview}
               stock={item.stock}
               images={item.images}
+              layout={layout}
             />
           );
         })}
