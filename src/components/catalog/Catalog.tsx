5bc097cdd88191a78c83884903a07fbc058c7b10
mkoroleva5 <mkoroleva5@gmail.com>
2022-12-16 18:53:25 +0300

    feat: add filter layout

--- src/components/catalog/Catalog.tsx
+++ src/components/catalog/Catalog.tsx
@@ -4,6 +4,7 @@ import { ProductCard } from './productCard/ProductCard';
 import products from '../../data/products.json';
 import { Layout } from './types';
 import { CatalogMenu } from './sortMenu/CatalogMenu';
+import { Filter } from './filter/Filter';
 
 export const Catalog = () => {
   const [layout, setLayout] = useState<Layout>('table');
@@ -15,21 +16,24 @@ export const Catalog = () => {
   return (
     <main className={style.main}>
       <div className={style.mainWrapper}>
-        <CatalogMenu onLayoutChange={changeLayout} />
-        <div className={`${style.catalogWrapper} ${layout === 'table' ? '' : style.list}`}>
-          {products.map((item) => {
-            return (
-              <ProductCard
-                key={item.id}
-                title={item.title}
-                price={item.price}
-                preview={item.preview}
-                stock={item.stock}
-                images={item.images}
-                layout={layout}
-              />
-            );
-          })}
+        <Filter />
+        <div className={style.catalogWrapper}>
+          <CatalogMenu onLayoutChange={changeLayout} />
+          <div className={`${style.productsWrapper} ${layout === 'table' ? '' : style.list}`}>
+            {products.map((item) => {
+              return (
+                <ProductCard
+                  key={item.id}
+                  title={item.title}
+                  price={item.price}
+                  preview={item.preview}
+                  stock={item.stock}
+                  images={item.images}
+                  layout={layout}
+                />
+              );
+            })}
+          </div>
         </div>
       </div>
     </main>
---
+++ src/components/catalog/filter/Filter.tsx
@@ -0,0 +1,10 @@
+import style from './Filter.module.css';
+
+export const Filter = () => {
+  return (
+    <div className={style.filterWrapper}>
+      <div className={style.title}>Фильтры</div>
+      <div className={style.field}>111</div>
+    </div>
+  );
+};
