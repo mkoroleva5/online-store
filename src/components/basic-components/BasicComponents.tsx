634eae4b571dbdca43fb7a4a5fcef90f4efc3269
mkoroleva5 <mkoroleva5@gmail.com>
2022-12-16 22:50:25 +0300

    feat: create options components

--- src/components/basic-components/BasicComponents.tsx
+++ src/components/basic-components/BasicComponents.tsx
@@ -21,3 +21,29 @@ export const Switch = ({ isOn, handleToggle }: SwitchProps) => {
     </>
   );
 };
+
+interface OptionType {
+  value: string;
+  id: number;
+}
+export const BrandOption = ({ value, id }: OptionType) => {
+  return (
+    <div className={style.option}>
+      <input className={style.optionInput} id={`brand-${id}`} type="checkbox" />
+      <label className={style.optionLabel} htmlFor={`brand-${id}`}>
+        {value}
+      </label>
+    </div>
+  );
+};
+
+export const ProductOption = ({ value, id }: OptionType) => {
+  return (
+    <div className={style.option}>
+      <input className={style.optionInput} id={`product-${id}`} type="checkbox" />
+      <label className={style.optionLabel} htmlFor={`product-${id}`}>
+        {value}
+      </label>
+    </div>
+  );
+};
--- src/components/catalog/filter/Filter.tsx
+++ src/components/catalog/filter/Filter.tsx
@@ -1,6 +1,10 @@
 import { useState } from 'react';
-import { Switch } from '../../basic-components/BasicComponents';
+import { BrandOption, ProductOption, Switch } from '../../basic-components/BasicComponents';
 import style from './Filter.module.css';
+import products from '../../../data/products.json';
+
+const brands = new Set(products.map((item): string => item.brand).sort());
+const productTypes = new Set(products.map((item): string => item.type).sort());
 
 export const Filter = () => {
   const [checked, setChecked] = useState(true);
@@ -8,21 +12,31 @@ export const Filter = () => {
     <div className={style.filterWrapper}>
       <div className={style.title}>Фильтры</div>
       <div className={style.field}>
-        <div className={`${style.option} ${style.inStock}`}>
-          <div className={style.optionTitle}>В наличии</div>
+        <div className={`${style.block} ${style.inStock}`}>
+          <div className={style.blockTitle}>В наличии</div>
           <Switch isOn={checked} handleToggle={() => setChecked(!checked)} />
         </div>
-        <div className={style.option}>
-          <div className={style.optionTitle}>Цена</div>
+        <div className={style.block}>
+          <div className={style.blockTitle}>Цена</div>
         </div>
-        <div className={style.option}>
-          <div className={style.optionTitle}>Количество на складе</div>
+        <div className={style.block}>
+          <div className={style.blockTitle}>Количество на складе</div>
         </div>
-        <div className={style.option}>
-          <div className={style.optionTitle}>Бренд</div>
+        <div className={style.block}>
+          <div className={style.blockTitle}>Бренд</div>
+          <div className={style.optionsWrapper}>
+            {[...brands].map((item, index) => {
+              return <BrandOption key={item} value={item} id={index} />;
+            })}
+          </div>
         </div>
-        <div className={style.option}>
-          <div className={style.optionTitle}>Тип продукта</div>
+        <div className={style.block}>
+          <div className={style.blockTitle}>Тип продукта</div>
+          <div className={style.optionsWrapper}>
+            {[...productTypes].map((item, index) => {
+              return <ProductOption key={item} value={item} id={index} />;
+            })}
+          </div>
         </div>
       </div>
     </div>
