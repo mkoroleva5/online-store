c5c6d25454e30c40676a3b2e96a47d023c09cc5c
mkoroleva5 <mkoroleva5@gmail.com>
2022-12-16 20:55:42 +0300

    feat: add switch button to stock option

--- src/components/catalog/filter/Filter.tsx
+++ src/components/catalog/filter/Filter.tsx
@@ -1,10 +1,51 @@
+import { useState } from 'react';
 import style from './Filter.module.css';
 
+type Props = {
+  isOn: boolean;
+  handleToggle: () => void;
+};
+
+const Switch = ({ isOn, handleToggle }: Props) => {
+  return (
+    <>
+      <input
+        checked={isOn}
+        onChange={handleToggle}
+        className={style.switchCheckbox}
+        id="switchNew"
+        type="checkbox"
+      />
+      <label className={`${style.switchLabel} ${isOn ? style.active : ''}`} htmlFor="switchNew">
+        <span className={style.switchButton} />
+      </label>
+    </>
+  );
+};
+
 export const Filter = () => {
+  const [checked, setChecked] = useState(true);
   return (
     <div className={style.filterWrapper}>
       <div className={style.title}>Фильтры</div>
-      <div className={style.field}>111</div>
+      <div className={style.field}>
+        <div className={`${style.option} ${style.inStock}`}>
+          <div className={style.optionTitle}>В наличии</div>
+          <Switch isOn={checked} handleToggle={() => setChecked(!checked)} />
+        </div>
+        <div className={style.option}>
+          <div className={style.optionTitle}>Цена</div>
+        </div>
+        <div className={style.option}>
+          <div className={style.optionTitle}>Количество на складе</div>
+        </div>
+        <div className={style.option}>
+          <div className={style.optionTitle}>Бренд</div>
+        </div>
+        <div className={style.option}>
+          <div className={style.optionTitle}>Тип продукта</div>
+        </div>
+      </div>
     </div>
   );
 };
