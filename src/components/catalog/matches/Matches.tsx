071fa2f1e2769cb3e6134d93369d8a31dd544e65
mkoroleva5 <mkoroleva5@gmail.com>
2022-12-21 15:18:58 +0300

    fix: refactor matches function

--- src/components/catalog/matches/Matches.tsx
+++ src/components/catalog/matches/Matches.tsx
@@ -6,9 +6,13 @@ interface MatchesProps {
 export const Matches = ({ length }: MatchesProps) => {
   let amount;
   const lastNum = +length.toString().split('').reverse()[0];
-  if (lastNum === 1 && length !== 11) amount = 'товар';
-  else if ((length < 10 || length > 20) && lastNum > 1 && lastNum < 5) amount = 'товара';
-  else amount = 'товаров';
+  if (lastNum === 1 && length !== 11) {
+    amount = 'товар';
+  } else if ((length < 10 || length > 20) && lastNum > 1 && lastNum < 5) {
+    amount = 'товара';
+  } else {
+    amount = 'товаров';
+  }
 
   const found = lastNum === 1 && length !== 11 ? 'Найден' : 'Найдено';
   return (
