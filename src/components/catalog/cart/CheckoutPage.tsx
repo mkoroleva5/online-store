5f9425ac491ba37bfba6502e3b2a87ce8d454333
mkoroleva5 <mkoroleva5@gmail.com>
2023-01-04 18:16:12 +0300

    feat: add form submit validation and hints for inputs

--- src/components/catalog/cart/CheckoutPage.tsx
+++ src/components/catalog/cart/CheckoutPage.tsx
@@ -16,6 +16,14 @@ interface CheckoutPageProps {
 
 export const CheckoutPage = ({ onClose }: CheckoutPageProps) => {
   const { dispatch } = useContext(CartState);
+  const [isEmptyName, setIsEmptyName] = useState(true);
+  const [isEmptyAddress, setIsEmptyAddress] = useState(true);
+  const [isEmptyEmail, setIsEmptyEmail] = useState(true);
+  const [isOrdered, setIsOrdered] = useState(false);
+
+  const nameRef = useRef<HTMLInputElement>(null);
+  const emailRef = useRef<HTMLInputElement>(null);
+  const addressRef = useRef<HTMLInputElement>(null);
 
   const { ref: phone, value: PHONE } = useIMask(
     {
@@ -85,15 +93,6 @@ export const CheckoutPage = ({ onClose }: CheckoutPageProps) => {
     return world;
   };
 
-  const [isEmptyName, setIsEmptyName] = useState(true);
-  const [isEmptyAddress, setIsEmptyAddress] = useState(true);
-  const [isEmptyEmail, setIsEmptyEmail] = useState(true);
-  const [isOrdered, setIsOrdered] = useState(false);
-
-  const nameRef = useRef<HTMLInputElement>(null);
-  const emailRef = useRef<HTMLInputElement>(null);
-  const addressRef = useRef<HTMLInputElement>(null);
-
   return (
     <div
       className={style.checkoutPageWrapper}
@@ -112,14 +111,26 @@ export const CheckoutPage = ({ onClose }: CheckoutPageProps) => {
         tabIndex={0}
       >
         {!isOrdered ? (
-          <form className={style.formWrapper}>
+          <form
+            className={style.formWrapper}
+            onSubmit={(e) => {
+              e.preventDefault();
+              setIsOrdered(true);
+              setTimeout(() => {
+                setIsOrdered(false);
+                dispatch({ type: 'CLEAR_CART' });
+                history.push('/');
+              }, 5000);
+            }}
+          >
             <div className={style.modalTitle}>Персональная информация</div>
             <div className={style.inputWrapper}>
               <input
                 ref={nameRef}
-                className={style.input}
+                className={classNames(style.input, { [style.emptyInput]: isEmptyName })}
                 pattern="[a-zA-Zа-яА-Я]{3,}(\s[a-zA-Zа-яА-Я]{3,})+"
                 type="text"
+                title="Имя должно содержать не менее двух слов, длина каждого не менее 3 символов"
                 onChange={(e) => {
                   const { value } = e.target;
                   if (value) {
@@ -128,6 +139,7 @@ export const CheckoutPage = ({ onClose }: CheckoutPageProps) => {
                     setIsEmptyName(true);
                   }
                 }}
+                required
               />
               <span className={classNames(style.inputTitle, { [style.empty]: isEmptyName })}>
                 Имя и Фамилия
@@ -136,9 +148,11 @@ export const CheckoutPage = ({ onClose }: CheckoutPageProps) => {
             <div className={style.inputWrapper}>
               <input
                 ref={phone}
-                className={style.input}
+                className={classNames(style.input, { [style.emptyInput]: !PHONE })}
                 pattern="^([+]?[\s0-9]+)?(\d{3}|[(]?[0-9]+[)])?([-]?[\s]?[0-9]){7}$"
                 type="text"
+                title="Номер телефона должен содержать 12 цифр"
+                required
               />
               <span className={classNames(style.inputTitle, { [style.empty]: !PHONE })}>
                 Телефон
@@ -147,9 +161,10 @@ export const CheckoutPage = ({ onClose }: CheckoutPageProps) => {
             <div className={style.inputWrapper}>
               <input
                 ref={addressRef}
-                className={style.input}
+                className={classNames(style.input, { [style.emptyInput]: isEmptyAddress })}
                 pattern="[a-zA-Zа-яА-Я]{5,}\s[a-zA-Zа-яА-Я]{5,}(\s[a-zA-Zа-яА-Я]{5,})+"
                 type="text"
+                title="Адрес должен содержать не менее трех слов, длина каждого не менее 5 символов"
                 onChange={(e) => {
                   const { value } = e.target;
                   if (value) {
@@ -158,6 +173,7 @@ export const CheckoutPage = ({ onClose }: CheckoutPageProps) => {
                     setIsEmptyAddress(true);
                   }
                 }}
+                required
               />
               <span className={classNames(style.inputTitle, { [style.empty]: isEmptyAddress })}>
                 Адрес доставки
@@ -166,9 +182,10 @@ export const CheckoutPage = ({ onClose }: CheckoutPageProps) => {
             <div className={style.inputWrapper}>
               <input
                 ref={emailRef}
-                className={style.input}
+                className={classNames(style.input, { [style.emptyInput]: isEmptyEmail })}
                 pattern="[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?"
                 type="e-mail"
+                title="E-mail должен содержать @ и иметь правильный домен"
                 onChange={(e) => {
                   const { value } = e.target;
                   if (value) {
@@ -177,6 +194,7 @@ export const CheckoutPage = ({ onClose }: CheckoutPageProps) => {
                     setIsEmptyEmail(true);
                   }
                 }}
+                required
               />
               <span className={classNames(style.inputTitle, { [style.empty]: isEmptyEmail })}>
                 E-mail
@@ -196,9 +214,11 @@ export const CheckoutPage = ({ onClose }: CheckoutPageProps) => {
                   <div className={style.inputWrapper}>
                     <input
                       ref={card}
-                      className={style.input}
+                      className={classNames(style.input, { [style.emptyInput]: !CARD })}
                       pattern="[0-9]{4}\s[0-9]{4}\s[0-9]{4}\s[0-9]{4}"
                       type="text"
+                      title="Количество введенных цифр должно быть ровно 16, допускается ввод только цифр"
+                      required
                     />
                     <span className={classNames(style.inputTitle, { [style.empty]: !CARD })}>
                       Номер карты
@@ -209,16 +229,25 @@ export const CheckoutPage = ({ onClose }: CheckoutPageProps) => {
                   <div className={style.inputWrapper}>
                     <input
                       ref={expire}
-                      className={style.input}
+                      className={classNames(style.input, { [style.emptyInput]: !EXPIRE })}
                       pattern="[0-9]{2}\/[0-9]{2}"
                       type="text"
+                      title="Месяц не может быть больше 12, год - меньше 23"
+                      required
                     />
                     <span className={classNames(style.inputTitle, { [style.empty]: !EXPIRE })}>
                       Годен до
                     </span>
                   </div>
                   <div className={style.inputWrapper}>
-                    <input ref={cvv} className={style.input} pattern="[0-9]{3}" type="text" />
+                    <input
+                      ref={cvv}
+                      className={classNames(style.input, { [style.emptyInput]: !CVV })}
+                      pattern="[0-9]{3}"
+                      type="text"
+                      title="Длина 3 символа, допускается ввод только цифр"
+                      required
+                    />
                     <span className={classNames(style.inputTitle, { [style.empty]: !CVV })}>
                       CVV
                     </span>
@@ -226,21 +255,7 @@ export const CheckoutPage = ({ onClose }: CheckoutPageProps) => {
                 </div>
               </div>
             </div>
-            <button
-              type="submit"
-              className={style.orderButton}
-              onClick={(e) => {
-                e.preventDefault();
-                setIsOrdered(true);
-                setTimeout(() => {
-                  setIsOrdered(false);
-                  dispatch({ type: 'CLEAR_CART' });
-                  history.push('/');
-                }, 3000);
-              }}
-            >
-              Оформить заказ
-            </button>
+            <input type="submit" className={style.orderButton} value="Оформить заказ" />
           </form>
         ) : (
           <div className={style.orderPageWrapper}>
