3c8e485ac548b2432fc918d8c07372d514e9b005
MOONcitizenX <mooncitizenx@gmail.com>
2023-01-07 12:41:12 +0300

    feat: add BackToTop button to catalog and cart

---
+++ src/components/basic-components/BackToTop.tsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from 'react';
+import style from './BackToTop.module.css';
+import arrow from '../../assets/icons/arrow.svg';
+
+export const BackToTop = () => {
+  const [isBackToTop, setIsBackToTop] = useState(false);
+
+  const listenerIsBackToTop = () => {
+    if (window.scrollY > 200) {
+      setIsBackToTop(true);
+    } else {
+      setIsBackToTop(false);
+    }
+  };
+
+  const scrollUp = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+    e.preventDefault();
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', listenerIsBackToTop);
+
+    return () => {
+      window.removeEventListener('scroll', listenerIsBackToTop);
+    };
+  }, []);
+
+  return (
+    <div>
+      {isBackToTop && (
+        <a href="#top" onClick={scrollUp}>
+          <div className={style.container}>
+            <img src={arrow} alt="Arrow" className={style.arrow} />
+          </div>
+        </a>
+      )}
+    </div>
+  );
+};
