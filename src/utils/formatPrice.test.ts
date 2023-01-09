64062e5d57101678cd8721612f5ed1ea48a54c4c
mkoroleva5 <mkoroleva5@gmail.com>
2023-01-09 23:30:57 +0300

    feat: write test for formatPrice function

---
+++ src/utils/formatPrice.test.ts
@@ -0,0 +1,16 @@
+import { formatPrice } from './formatPrice';
+
+describe('FormatPrice function test', () => {
+  it('Function returns "2.50"', () => {
+    expect(formatPrice(2.5)).toBe('2.50');
+  });
+  it('Function returns "36.00"', () => {
+    expect(formatPrice(36)).toBe('36');
+  });
+  it('Function returns "75.90"', () => {
+    expect(formatPrice(75.9)).toBe('75.90');
+  });
+  it.skip('Function returns "25.45"', () => {
+    expect(formatPrice(25.4545875)).toBe('25.45');
+  });
+});
