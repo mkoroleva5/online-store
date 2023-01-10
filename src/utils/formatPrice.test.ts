b83de401e915967147182ccd855140a87d81abc7
MOONcitizenX <mooncitizenx@gmail.com>
2023-01-10 18:33:27 +0300

    fix: fix format price tests

--- src/utils/formatPrice.test.ts
+++ src/utils/formatPrice.test.ts
@@ -5,12 +5,12 @@ describe('FormatPrice function test', () => {
     expect(formatPrice(2.5)).toBe('2.50');
   });
   it('Function returns "36.00"', () => {
-    expect(formatPrice(36)).toBe('36');
+    expect(formatPrice(36)).toEqual('36.00');
   });
   it('Function returns "75.90"', () => {
     expect(formatPrice(75.9)).toBe('75.90');
   });
-  it.skip('Function returns "25.45"', () => {
+  it('Function returns "25.45"', () => {
     expect(formatPrice(25.4545875)).toBe('25.45');
   });
 });
