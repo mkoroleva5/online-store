a84d09520bdd31a2d3aa98e1eedae7954441f778
mkoroleva5 <mkoroleva5@gmail.com>
2023-01-11 20:45:37 +0300

    fix: make countItems function separate https://github.com/mkoroleva5/online-store/pull/65/files/5536210a39533f7bff8f6007a7ca49f9ba80058d#r1060878207

--- src/utils/countItems.ts
+++ src/utils/countItems.ts
@@ -1,10 +1,9 @@
-export const countItems = (length: number) => {
-  const lastNum = +length.toString().split('').reverse()[0];
-  if (lastNum === 1 && length !== 11) {
-    return 'товар';
+export const countItems = (length: number, array: string[]) => {
+  if (length % 10 === 1 && length % 100 !== 11) {
+    return array[0];
   }
-  if ((length < 10 || length > 20) && lastNum > 1 && lastNum < 5) {
-    return 'товара';
+  if ((length < 10 || length > 20) && length % 10 > 1 && length % 10 < 5) {
+    return array[2];
   }
-  return 'товаров';
+  return array[1];
 };
